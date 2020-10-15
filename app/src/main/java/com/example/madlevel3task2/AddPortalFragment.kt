10b04a7a45c6dbc714203613d3fdf057d5863e26
Koen Groot <kwgroot@gmail.com>
2020-10-15 13:42:33 +0200

    Navigation using fab is now possible, portals cannot be added yet.

--- app/src/main/java/com/example/madlevel3task2/AddPortalFragment.kt
+++ app/src/main/java/com/example/madlevel3task2/AddPortalFragment.kt
@@ -7,25 +7,30 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_add_portal.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class AddPortalFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.fragment_add_portal, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        btnAddPortal.setOnClickListener{
+            onAddPortal()
         }
+    }
+
+    private fun onAddPortal() {
+        findNavController().popBackStack()
     }
 }
