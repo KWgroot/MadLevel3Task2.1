3e160e71d29335c0c7b3e785173f6d78dab06e61
Koen Groot <kwgroot@gmail.com>
2020-10-15 14:18:39 +0200

    Made sure the URL is checked to be an actual URL.

--- app/src/main/java/com/example/madlevel3task2/AddPortalFragment.kt
+++ app/src/main/java/com/example/madlevel3task2/AddPortalFragment.kt
@@ -1,13 +1,17 @@
 package com.example.madlevel3task2
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.URLUtil
 import android.widget.Button
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_add_portal.*
+import java.net.URL
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -31,6 +35,18 @@ class AddPortalFragment : Fragment() {
     }
 
     private fun onAddPortal() {
-        findNavController().popBackStack()
+        val URLTitle = txtInputTitle.text.toString()
+        val validURL = isValidURL(txtInputURL.text.toString())
+
+        if (URLTitle.isNotBlank() && validURL) {
+            //Add to recycleview
+            findNavController().popBackStack()
+        } else {
+            Toast.makeText(activity, "Fill in a title and valid URL", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun isValidURL(urlToCheck: String): Boolean {
+        return URLUtil.isValidUrl(urlToCheck) && Patterns.WEB_URL.matcher(urlToCheck).matches()
     }
 }
